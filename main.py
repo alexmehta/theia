5a0abef14e4fcb8616491ed5acc61bf4eb0295b3
WarPromo <kooltyme1@gmail.com>
2023-02-03 22:08:02 -0800

    different files

--- main.py
+++ main.py
@@ -6,36 +6,24 @@ import pygame
 import time
 import json
 
+from drawnotes import NoteDrawer;
+from noteplayer import NotePlayer
+from downsampled import GenerateDownsampled
+from objectdownsampled import GenerateObjectDownsampled
+from get_soundindex import get_soundindex;
+
 pygame.init();
 pygame.midi.init()
 pygame.font.init()
 pygame.mixer.init();
 
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
-output = pygame.midi.Output(0)
-
-def drum(pitch, volume, pan):
-    output.write_short(192, 117);
-    output.write_short(176, 10, pan);
-    output.write_short(0x90, pitch, volume)
-    output.write_short(192, 0);
-
-def playnote(pitch, volume, pan):
-
-    output.write_short(176, 10, pan);
-    output.write_short(0x90, pitch, volume)
-
-def offnote(pitch, volume):
-    output.write_short(128, pitch, volume);
-
 
 pipeline = rs.pipeline()
 config = rs.config()
 
 resx = 640;
 resy = 480;
-
-
 
 
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
@@ -56,18 +44,6 @@ def loadsoundsettings():
             soundfiles[classname] = pygame.mixer.Sound("./sounds/"+classname+".mp3");
 loadsoundsettings();
 
-
-
-
-print(soundsettings);
-
-
-def get_soundindex(distance):
-
-    if not (soundsettings["mindistance"] <= distance <= soundsettings["maxdistance"]): return None;
-
-    return int( soundsettings["magnitude"] * ( (distance - soundsettings["mindistance"]) / (soundsettings["maxdistance"] - soundsettings["mindistance"]) )**soundsettings["smoothen"] )
-
 def get_boundingboxes():
     return [[90,120,90,120, "dog"],[200,190,200,190, "person"],[300,50,300,50, "chair"]] #x1,y1,x2,y2
 
@@ -85,23 +61,20 @@ class Model:
         self.lastnote = None;
         self.repeated = False;
 
-        self.thetamax = 45;
-
         self.xskip = 40;
         self.yskip = 40;
 
-        self.downsampled = [];
-        self.downsampledmap = [];
+        self.sx = int(resx / self.xskip);
+        self.sy = int(resy / self.yskip);
 
-        self.objectdownsampled = [];
-        self.objectdownsampledmap = [];
+        self.generate_downsampled = GenerateDownsampled(5, self.xskip, self.yskip, resx, resy, soundsettings)
+        self.generate_object_downsampled = GenerateObjectDownsampled(self.xskip, self.yskip, resx, resy)
+        self.note_drawer = NoteDrawer(pygame, surface, 0, 0, 320, 240, self.sx, self.sy)
+        self.note_player = NotePlayer(pygame);
 
     def draw(self):
 
         if(self.ticks % soundsettings["setpointinterval"] == 0 or pygame.key.get_pressed()[pygame.K_SPACE]):
-
-
-            print("Redraw");
 
             loadsoundsettings();
 
@@ -118,90 +91,31 @@ class Model:
 
             frames = pipeline.wait_for_frames()
             depth_frame = frames.get_depth_frame()
-            #color_frame = frames.get_color_frame()
 
-            #color_image = np.asanyarray(color_frame.get_data())
-
-            #pc.map_to(color_frame)
-
-            points = np.asanyarray(depth_frame.get_data());
-
-            for x in range(int(resx / self.xskip)-1, -1, -1):
-                for y in range(0, int(resy / self.yskip)):
-                    depth = depth_frame.get_distance(x * self.xskip, y * self.yskip);
-
-
-                    foundinbound = False;
-
-                    for j in range(-soundsettings["checkrange"],soundsettings["checkrange"]):
-
-                        if foundinbound: break;
-
-                        for k in range(-soundsettings["checkrange"],soundsettings["checkrange"]):
-
-                            if foundinbound: break;
-
-                            if x * self.xskip + j >= resx or x * self.xskip + j < 0: break;
-                            if y * self.yskip + k >= resy or y * self.yskip + k < 0: break;
-
-                            depth = depth_frame.get_distance(x * self.xskip + j, y * self.yskip + k);
-
-                            inbound = get_soundindex(depth) != None;
-
-                            if inbound:
-                                self.downsampledmap.append( int(resx / self.xskip) - 1 - x )
-                                self.downsampledmap.append( y )
-                                self.downsampledmap.append( depth )
-                                self.downsampled.append( depth );
-                                foundinbound = True;
-
-                    if not foundinbound: self.downsampled.append(0);
-
-            for x in range(0, len(self.downsampled)):
-                self.objectdownsampled.append(0);
-
-
-            objects = get_boundingboxes();
-
-            for object in objects:
-
-                x = (object[0] + object[2])/2;
-                y = (object[1] + object[3])/2;
-
-                x = int(x / self.xskip);
-                y = int(y / self.yskip);
-                index = y + x * int(resy / self.yskip);
-
-                self.objectdownsampled[index] = object[4];
-                self.objectdownsampledmap.append(x);
-                self.objectdownsampledmap.append(y);
-                self.objectdownsampledmap.append(object[4]);
-
-
+            (self.downsampled, self.downsampledmap) = self.generate_downsampled.generate(depth_frame);
+            (self.objectdownsampled, self.objectdownsampledmap) = self.generate_object_downsampled.generate(get_boundingboxes());
 
         if( (self.ticks % soundsettings["setpointinterval"]) % soundsettings["soundtickinterval"] == 0 and self.soundtick < len(self.downsampled) ):
 
+            soundindex = get_soundindex(self.downsampled[self.soundtick], soundsettings);
 
-            soundindex = get_soundindex(self.downsampled[self.soundtick]);
-
-            y = self.soundtick % (resy / self.yskip);
-            x = int(self.soundtick / (resy / self.yskip));
+            y = self.soundtick % self.sy;
+            x = int(self.soundtick / self.sy);
 
             self.soundpoint = (x, y);
 
-
-            x = x / ( (resx / self.xskip) - 1)
-            x = 128 - int(x * 128);
+            pan = x / ( self.sx - 1)
+            pan = int(pan * 128);
+            print(pan);
 
             if(self.lastnote != None):
-                offnote(self.lastnote, 0);
+                self.note_player.offnote(self.lastnote, 0);
 
-
-            dorepeat = int(self.soundtick / (resy / self.yskip)) > int( (self.soundtick-1) / (resy / self.yskip))
+            dorepeat = int(self.soundtick / self.sy) > int( (self.soundtick-1) / self.sy)
             if(dorepeat and self.repeated == False):
 
                 self.repeated = True;
-                drum(70, 70, x);
+                self.note_player.drum(70, 70, pan);
                 self.soundtick -= 1;
 
             elif(soundindex != None):
@@ -209,18 +123,15 @@ class Model:
                 pitch = soundsettings["startnote"] + soundindex * soundsettings["deltanote"]
                 volume = soundsettings["startvolume"] + soundindex * soundsettings["deltavolume"];
 
-                playnote( pitch, volume, x)
+                self.note_player.playnote( pitch, volume, pan)
 
                 self.lastnote = pitch;
                 self.repeated = False;
 
             elif(soundindex == None):
 
-                drum(60, 20, x);
+                self.note_player.drum(60, 20, pan);
                 self.repeated = False;
-
-
-
 
             self.soundtick += 1;
 
@@ -233,25 +144,22 @@ class Model:
            self.ticks % soundsettings["setpointinterval"] > self.endsoundtick + soundsettings["speakingaftergriddelay"] and
            self.ticks % soundsettings["setpointinterval"] > self.ticklimiter):
 
-            y = self.voicetick % (resy / self.yskip);
-            x = int(self.voicetick / (resy / self.yskip));
+            y = self.voicetick % self.sy;
+            x = int(self.voicetick / self.sy);
 
             self.soundpoint = (x, y);
 
+            pan = x / ( self.sx - 1)
+            pan = int(pan * 128);
 
-            x = x / ( (resx / self.xskip) - 1)
-            x = 128 - int(x * 128);
-
-            dorepeat = int(self.voicetick / (resy / self.yskip)) > int( (self.voicetick-1) / (resy / self.yskip))
+            dorepeat = int(self.voicetick / self.sy) > int( (self.voicetick-1) / self.sy)
             if(dorepeat and self.repeated == False):
 
                 self.repeated = True;
-                drum(70, 70, x);
+                self.note_player.drum(70, 70, pan);
                 self.voicetick -= 1;
 
             elif(self.objectdownsampled[self.voicetick] != 0):
-
-                print("play");
 
                 sound = soundfiles[self.objectdownsampled[self.voicetick]];
                 sound.play();
@@ -259,62 +167,17 @@ class Model:
                 self.ticklimiter = self.ticks % soundsettings["setpointinterval"] + soundsettings["speakingdelay"];
 
             else:
-
-                drum(60, 20, x);
+                self.note_player.drum(60, 20, pan);
                 self.repeated = False;
-
-
-
 
             self.voicetick += 1;
 
-
-
-
-
-        s = 16 * 20;
-
-        pygame.draw.rect(surface, (0,255,0), pygame.Rect(0,0,s,int((resy/resx) * s)))
-
-
-
         if(self.soundtick < len(self.downsampled)):
-            for i in range(0, int(len(self.downsampledmap)/3)):
-
-                w = (s / (resx / self.xskip));
-                x = s - self.downsampledmap[i*3] / (resx / self.xskip) * s - w;
-                y = self.downsampledmap[i*3 + 1] / (resx / self.xskip) * s;
-
-
-                soundindex = get_soundindex(self.downsampledmap[i*3 + 2]);
-
-                color = 255 - soundindex * (255 / soundsettings["magnitude"]);
-
-                if(color < 0): color = 0;
-                if(color > 255): color = 255;
-
-                pygame.draw.rect(surface, (color,0,0), pygame.Rect(x,y,w,w));
+            self.note_drawer.draw_notes(self.downsampledmap, soundsettings["maxdistance"], soundsettings["mindistance"], 0, 255);
         else:
-            for i in range(0, int(len(self.objectdownsampledmap)/3)):
+            self.note_drawer.draw_objects(self.objectdownsampledmap);
 
-                w = (s / (resx / self.xskip));
-                x = s - self.objectdownsampledmap[i*3] / (resx / self.xskip) * s - w;
-                y = self.objectdownsampledmap[i*3 + 1] / (resx / self.xskip) * s;
-
-                color = 255;
-
-                pygame.draw.rect(surface, (color,0,0), pygame.Rect(x,y,w,w));
-
-        if(self.soundpoint != None):
-
-            w = (s / (resx / self.xskip));
-            x = s - self.soundpoint[0] / (resx / self.xskip) * s - w;
-            y = self.soundpoint[1] / (resx / self.xskip) * s;
-
-
-            pygame.draw.rect(surface, (255,255,255), pygame.Rect(x,y,w,w));
-
-
+        self.note_drawer.draw_soundpoint(self.soundpoint);
 
         self.ticks += 1;
 
---
+++ noteplayer.py
@@ -0,0 +1,21 @@
+import pygame.midi;
+
+class NotePlayer():
+
+    def __init__(self, pygame):
+        pygame.midi.init()
+        self.output = output = pygame.midi.Output(0)
+
+    def drum(self, pitch, volume, pan):
+        self.output.write_short(192, 117);
+        self.output.write_short(176, 10, pan);
+        self.output.write_short(0x90, pitch, volume)
+        self.output.write_short(192, 0);
+
+    def playnote(self, pitch, volume, pan):
+
+        self.output.write_short(176, 10, pan);
+        self.output.write_short(0x90, pitch, volume)
+
+    def offnote(self, pitch, volume):
+        self.output.write_short(128, pitch, volume);
